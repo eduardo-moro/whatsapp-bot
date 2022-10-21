9e521c39ee81c440d1f98831b76550155676fb66
eduardo-moro <eduardomoro1127@gmail.com>
2022-10-20 21:30:28 -0300

    uploading project

---
+++ app.js
@@ -0,0 +1,60 @@
+
+const express = require('express');
+const fs = require('fs');
+const createError = require('http-errors');
+const morgan = require('morgan');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+const qrcode = require('qrcode-terminal')
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+require('dotenv').config();
+
+const app = express();
+
+const client = new Client({
+    puppeteer: { headless: true },
+    authStrategy: new LocalAuth(),
+});
+
+client.initialize();
+
+client.on('qr', qr => {
+  // NOTE: This event will not be fired if a session is specified.
+  console.log('QR RECEIVED', qr);
+  qrcode.generate(qr, { small: true }); // Add this line
+  app.get('/getqr', (req, res, next) => {
+    res.send({ qr });
+  });
+});
+
+client.on('authenticated', session => {
+  console.log('AUTHENTICATED', session);
+});
+
+client.on('auth_failure', msg => {
+  // Fired if session restore was unsuccessfull
+  console.error('AUTHENTICATION FAILURE', msg);
+});
+
+client.on('ready', () => {
+  console.log('READY');
+});
+
+app.post('/sendmessage', jsonParser, async (req, res) => {
+  try {
+    await console.log(req.body);
+    const { number, message } = req.body; // Get the body
+    const msg = await client.sendMessage(`${number}@c.us`, message); // Send the message
+    res.send({ msg }); // Send the response
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Listening for the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
+
